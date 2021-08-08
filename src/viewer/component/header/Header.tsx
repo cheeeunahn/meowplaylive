82dc0c77f1be833c021104d8bf52421ae7cfc56e
Avantgarde95 <95phm@kaist.ac.kr>
2021-08-08 16:49:30 +0900

    [feat] 헤 더 고 쳤 다
    * 기존: scroll 생길 시 헤더 배경 잘림
    * absolute로 겹친 헤더 배경이랑 진짜 헤더 배경이랑 겹쳐서 해결
    * 남자는 box-shadow 따위 무시한다
    
    (>.<)
    <)  )/
     /  \
    
    (>.<)
    \(  )>
     /  \
    
    (>.<)
    \(  )/
     /  \
    
    Dancing

--- src/viewer/component/header/Header.tsx
+++ src/viewer/component/header/Header.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { css } from '@emotion/css';
 
-import { commonColors } from 'component/Common';
+import { CommonBox, commonColors } from 'component/Common';
 import { ExitButton } from 'component/header/ExitButton';
 import { NicknameEditor } from 'component/header/NicknameEditor';
 
 export const Header = () => (
-    <div className={css({
+    <CommonBox className={css({
         zIndex: 3,
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'flex-end',
         boxSizing: 'border-box',
         width: '100%',
-        padding: '2rem 2rem 1em 2rem',
-        color: commonColors.white
+        height: '7rem',
+        paddingBottom: '1.2rem',
+        boxShadow: 'none',
+        color: commonColors.white,
+        backgroundColor: commonColors.black
     })}>
         <div>
             <div className={css({
@@ -32,5 +35,5 @@ export const Header = () => (
             <NicknameEditor />
             <ExitButton />
         </div>
-    </div>
+    </CommonBox>
 );
