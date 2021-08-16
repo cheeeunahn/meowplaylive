c0fb0e193150f52ee56db8516299156d4c055c8b
Avantgarde95 <95phm@kaist.ac.kr>
2021-08-16 13:27:13 +0900

    [feat] Prevent nickname from getting too long
    * Check nickname's length in NicknameEditor

--- viewer/src/common/StringUtils.ts
+++ viewer/src/common/StringUtils.ts
@@ -27,7 +27,40 @@ export function timestampToString(value: number) {
     return `${year}.${month}.${day}-${hour}.${minutes}.${seconds}`;
 }
 
+function isHangulChar(ch: string) {
+    const c = ch.charCodeAt(0);
+
+    if (0x1100 <= c && c <= 0x11FF) {
+        return true;
+    }
+
+    if (0x3130 <= c && c <= 0x318F) {
+        return true;
+    }
+
+    if (0xAC00 <= c && c <= 0xD7A3) {
+        return true;
+    }
+
+    return false;
+}
+
 export function validateNickname(nickname: string) {
+    // Nickname shouldn't be too long.
+    let length = 0;
+
+    for (let i = 0; i < nickname.length; i++) {
+        if (isHangulChar(nickname[i])) {
+            length += 2;
+        } else {
+            length += 1;
+        }
+    }
+
+    if (length > 13) {
+        return false;
+    }
+
     // Allow: English/Korean characters, numbers, underlines, hyphens and spaces.
     // Nickname shouldn't start with a space.
     return /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9_\-][a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9_\-\s]*$/g.test(nickname);
