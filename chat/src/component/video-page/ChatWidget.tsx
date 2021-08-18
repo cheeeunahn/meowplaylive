c57940fd13c08d715b53f3ee578c7101db09d53f
Avantgarde95 <95phm@kaist.ac.kr>
2021-08-18 12:48:03 +0900

    [feat] Chat point system
    * Chat point system
      * Reuse viewer point system code
      * Rename the DB files (for avoiding confusion)
        * DB related to viewer & cat ui: viewer-xxx-database.db
        * DB related to chat ui: chat-xxx-database.db

--- chat/src/component/video-page/ChatWidget.tsx
+++ chat/src/component/video-page/ChatWidget.tsx
@@ -4,18 +4,24 @@ import { css } from '@emotion/css';
 import { CommonBox } from 'component/Common';
 import { ChatViewer } from 'component/video-page/ChatViewer';
 import { ChatSender } from 'component/video-page/ChatSender';
+import { PointView } from 'component/video-page/PointView';
 
 export const ChatWidget = () => {
     return (
-        <CommonBox className={css({
-            display: 'inline-flex',
-            flexDirection: 'column',
-            width: '720px', /*30rem*/
-            height: '380px', /*20rem*/
-            padding: '0'
+        <div className={css({
+            marginBottom: '2rem'
         })}>
-            <ChatViewer />
-            <ChatSender />
-        </CommonBox>
+            <CommonBox className={css({
+                display: 'inline-flex',
+                flexDirection: 'column',
+                width: '720px', /*30rem*/
+                height: '380px', /*20rem*/
+                padding: '0'
+            })}>
+                <ChatViewer />
+                <ChatSender />
+            </CommonBox>
+            <PointView />
+        </div>
     );
 };
